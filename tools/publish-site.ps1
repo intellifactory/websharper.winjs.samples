00f5271e5f46f2ce438534bb264aed49b8cb0d80
Sandor Rakonczai <lunnia@inf.elte.hu>
2015-04-03 11:18:16 +0200

    Emptied branch

--- tools/publish-site.ps1
+++
@@ -1,8 +0,0 @@
-# creates build/html
-rm -r build -errorAction ignore
-$d = mkdir build
-$d = mkdir build/html
-cp -r Content build/html/
-cp -r *.css build/html/
-cp -r *.html build/html/
-cp -r Resources build/html
